32e393b85a6feb70b793afcf5183be21beac7397
istvan.foldy <istvan.foldy@innonic.com>
2020-05-08 19:16:54 +0200

    #3 validation for inputs

--- insert.php
+++ insert.php
@@ -4,36 +4,51 @@ error_reporting(0);
 ?>
 <div class="row">
     <div class="col-6">
-        <form action="" method="GET">
+        <form class="mp-needs-validation" action="" method="GET" novalidate>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text font-weight-bold" id="productNameId">Termék neve:</span>
                 </div>
                 <input type="text" name="productName" value="" class="form-control" aria-describedby="productNameId" required>
+                <div class="invalid-feedback">
+                    Mező kitöltése kötelező
+                </div>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text font-weight-bold" id="productDescId">Termék leírása:</span>
                 </div>
                 <textarea type="text" name="productDesc" value="" class="form-control" aria-describedby="productDescId" required></textarea>
+                <div class="invalid-feedback">
+                    Mező kitöltése kötelező
+                </div>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text font-weight-bold" id="productGrossPriceId">Bruttó ár:</span>
                 </div>
                 <input type="text" name="grossPrice" value="" class="form-control" aria-describedby="productGrossPriceId" required>
+                <div class="invalid-feedback">
+                    Mező kitöltése kötelező
+                </div>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text font-weight-bold" id="productSkuId">Cikkszám:</span>
                 </div>
                 <input type="text" name="sku" value="" class="form-control" aria-describedby="productSkuId" required>
+                <div class="invalid-feedback">
+                    Mező kitöltése kötelező
+                </div>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text font-weight-bold" id="productQuantitySumId">Mennyiség:</span>
                 </div>
                 <input type="text" name="quantitySum" value="" class="form-control" aria-describedby="productQuantitySumId" required>
+                <div class="invalid-feedback">
+                    Mező kitöltése kötelező
+                </div>
             </div>
             <div class="d-flex justify-content-end mb-3">
                 <input type="submit" class="btn btn-primary" name="submit" value="Mentés">
--- update.php
+++ update.php
@@ -16,8 +16,8 @@ $_GET['quantitySum'];
         <?php echo $_GET['productName']; ?>
     </div>
     <div class="col-6">
-        <form action="" method="GET">
-            <div class="input-group mb-3">
+        <form action="" class="mp-needs-validation" method="GET" novalidate>
+            <div class="mb-0">
                 <input name="id" value="<?php echo $_GET['id']; ?>" class="form-control" aria-describedby="productId" type="hidden">
             </div>
             <div class="input-group mb-3">
@@ -25,30 +25,45 @@ $_GET['quantitySum'];
                     <span class="input-group-text font-weight-bold" id="productNameId">Termék neve:</span>
                 </div>
                 <input type="text" name="productName" value="<?php echo $_GET['productName']; ?>" class="form-control" aria-describedby="productNameId" required>
+                <div class="invalid-feedback">
+                    Mező kitöltése kötelező
+                </div>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text font-weight-bold" id="productDescId">Termék leírása:</span>
                 </div>
                 <textarea type="text" name="productDesc" value="<?php echo $_GET['productDesc']; ?>" class="form-control" aria-describedby="productDescId" required><?php echo $_GET['productDesc']; ?></textarea>
+                <div class="invalid-feedback">
+                    Mező kitöltése kötelező
+                </div>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text font-weight-bold" id="productGrossPriceId">Bruttó ár:</span>
                 </div>
                 <input type="text" name="grossPrice" value="<?php echo $_GET['grossPrice']; ?>" class="form-control" aria-describedby="productGrossPriceId" required>
+                <div class="invalid-feedback">
+                    Mező kitöltése kötelező
+                </div>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text font-weight-bold" id="productSkuId">Cikkszám:</span>
                 </div>
                 <input type="text" name="sku" value="<?php echo $_GET['sku']; ?>" class="form-control" aria-describedby="productSkuId" required>
+                <div class="invalid-feedback">
+                    Mező kitöltése kötelező
+                </div>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text font-weight-bold" id="productQuantitySumId">Mennyiség:</span>
                 </div>
                 <input type="text" name="quantitySum" value="<?php echo $_GET['quantitySum']; ?>" class="form-control" aria-describedby="productQuantitySumId" required>
+                <div class="invalid-feedback">
+                    Mező kitöltése kötelező
+                </div>
             </div>
             <div class="d-flex justify-content-end mb-3">
                 <input class="btn btn-primary" type="submit" name="submit" value="Frissítés">
